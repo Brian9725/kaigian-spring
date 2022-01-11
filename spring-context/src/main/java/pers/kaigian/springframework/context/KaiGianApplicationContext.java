8e417df8bc4c02e38f401a6346f1c0086cf17546
Kaiyang Hu <1786104060@qq.com>
2022-01-11 19:58:32 +0800

    添加日志支持

--- spring-context/src/main/java/pers/kaigian/springframework/context/KaiGianApplicationContext.java
+++ spring-context/src/main/java/pers/kaigian/springframework/context/KaiGianApplicationContext.java
@@ -1,10 +1,17 @@
 package pers.kaigian.springframework.context;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 自己的Spring上下文环境
  *
  * @author BrianHu
  * @create 2021-09-03 22:22
  **/
+@Slf4j
 public class KaiGianApplicationContext extends ApplicationContext {
+
+    public KaiGianApplicationContext() {
+        log.info("kaigian");
+    }
 }
--- spring-test/src/main/java/pers/kaigian/springframework/Test.java
+++ spring-test/src/main/java/pers/kaigian/springframework/Test.java
@@ -1,11 +1,14 @@
 package pers.kaigian.springframework;
 
+import lombok.extern.slf4j.Slf4j;
+import pers.kaigian.springframework.context.KaiGianApplicationContext;
+
 /**
  * @author BrianHu
  * @create 2021-09-03 22:16
  **/
 public class Test {
     public static void main(String[] args) {
-
+        new KaiGianApplicationContext();
     }
 }
