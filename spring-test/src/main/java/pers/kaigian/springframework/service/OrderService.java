4a27ce96ec8077bfd5dc0b6ab508745747ac8cca
Kaiyang Hu <1786104060@qq.com>
2022-01-12 21:58:44 +0800

    添加OrderService

---
+++ spring-test/src/main/java/pers/kaigian/springframework/service/OrderService.java
@@ -0,0 +1,11 @@
+package pers.kaigian.springframework.service;
+
+import pers.kaigian.springframework.annotation.Component;
+
+/**
+ * @author BrianHu
+ * @create 2022-01-12 21:58
+ **/
+@Component
+public class OrderService {
+}
