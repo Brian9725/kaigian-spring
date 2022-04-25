2628527a2f43121b7ced2c3221f9667c37a59d0c
BrianHu <1786104060@qq.com>
2022-04-25 17:43:45 +0800

    添加BeanDefinition接口和AbstractBeanDefinition抽象类

---
+++ spring-core/src/main/java/pers/kaigian/springframework/utils/ClassUtils.java
@@ -0,0 +1,25 @@
+package pers.kaigian.springframework.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author BrianHu
+ * @create 2022-04-25 17:02
+ **/
+public class ClassUtils {
+
+    private static final Map<String, Class<?>> classMap = new HashMap<>(256);
+
+    public static Class<?> forName(String className, ClassLoader classLoader) throws ClassNotFoundException {
+        if (classMap.containsKey(className)) {
+            return classMap.get(className);
+        }
+        if (classLoader == null) {
+            classLoader = ClassLoaderUtils.getSystemClassLoader();
+        }
+        Class<?> loadClass = classLoader.loadClass(className);
+        classMap.put(className, loadClass);
+        return loadClass;
+    }
+}
